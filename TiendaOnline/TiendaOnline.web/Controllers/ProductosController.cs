5d87cca7c9130e7e6c83123eaf9627db3f0d2a0b
Kesia Peralta <marilyn16.peralta@gmail.com>
2021-09-27 22:52:58 -0600

    controler producto

---
+++ TiendaOnline/TiendaOnline.web/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaOnline.web.Models;
+
+namespace TiendaOnline.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 1;
+            producto.Descripcion = "Camisa Polo";
+
+            return View(producto);
+        }
+    }
+}
