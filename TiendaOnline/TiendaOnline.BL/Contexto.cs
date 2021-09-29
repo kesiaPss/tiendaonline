9f924184efc3f696bde0564b00f9a7edeec9dbe7
Kesia Peralta <marilyn16.peralta@gmail.com>
2021-09-28 19:10:33 -0600

    Conecta con la base de datos

--- TiendaOnline/TiendaOnline.BL/Contexto.cs
+++ TiendaOnline/TiendaOnline.BL/Contexto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,14 @@ namespace TiendaOnline.BL
 {
     public class Contexto: DbContext
     {
-        public Contexto(): base("TiendaOnlineDB")
+        public Contexto(): base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\TiendaOnlineDB.mdf")
         {
 
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
         public DbSet<Producto> Productos { get; set; }
     }
--- TiendaOnline/TiendaOnline.BL/ProductosBL.cs
+++ TiendaOnline/TiendaOnline.BL/ProductosBL.cs
@@ -16,29 +16,8 @@ namespace TiendaOnline.BL
         }
         public List<Producto> ObtenerProductos()
         {
-            _contexto.Productos.ToList();
+           return _contexto.Productos.ToList();
 
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Camisa Polo";
-            producto1.Precio = 280;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Camisa Licra";
-            producto2.Precio = 130;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Jeans licra";
-            producto3.Precio = 430;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
         }
     }
 }
