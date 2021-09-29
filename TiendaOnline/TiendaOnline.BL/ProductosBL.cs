96006d95121e3c418372a66a0edc6773678b889b
Kesia Peralta <marilyn16.peralta@gmail.com>
2021-09-28 18:07:08 -0600

    Agregando Arquitectura Basica

---
+++ TiendaOnline/TiendaOnline.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnline.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Camisa Polo";
+            producto1.Precio = 280;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Camisa Licra";
+            producto2.Precio = 130;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Jeans licra";
+            producto3.Precio = 430;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
--- TiendaOnline/TiendaOnline.web/Controllers/ProductosController.cs
+++ TiendaOnline/TiendaOnline.web/Controllers/ProductosController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TiendaOnline.web.Models;
+using TiendaOnline.BL;
 
 namespace TiendaOnline.web.Controllers
 {
@@ -12,22 +12,8 @@ namespace TiendaOnline.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Camisa Polo";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Camisa Licra";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Jeans licra";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
